b2912f010cf283e8a0a040ad5297431a33776027
nigh7sh4de <plotnikd@gmail.com>
2022-03-26 19:48:37 -0700

    misc

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+declare const global: { HermesInternal: null | {} };
+
+const App = () => {
+  return (
+    <div style={{ flexGrow: 1 }}>
+      <p>I am the app :D</p>
+    </div>
+  );
+};
+
+export default App;
