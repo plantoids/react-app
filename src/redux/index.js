c2cba161d979b5fde607eee189e4b1856261dc48
nigh7sh4de <plotnik@gmail.com>
2022-03-27 11:28:20 -0700

    cleanup unused files

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { createLogger } from "redux-logger";
 
 export const store = configureStore({
