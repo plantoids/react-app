759c19bf81e6b64b31348c6e01f2351885be538a
nigh7sh4de <plotnik@gmail.com>
2022-03-31 15:15:26 -0700

    react router init

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { HomePage } from "src/components/home";
 
 declare const global: { HermesInternal: null | {} };
 
 const App = () => {
   return (
     <div style={{ flexGrow: 1 }}>
-      <p>I am the app :D</p>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
