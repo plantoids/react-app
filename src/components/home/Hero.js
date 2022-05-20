7baf9c031b218e1dc3c505a21ba2dc2e85b98acc
madhouse1 <ognen_petrovski@hotmail.com>
2022-05-20 13:54:05 +0200

    video-hero, fix border-radius

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -55,7 +55,7 @@ const Hero = () => {
             </div>
           </div>
           <div className="hero__card">
-            <Card kg="1.69" age="54">
+            <Card kg="1.69" age="54" activeImage={"./assets/growing-plantoid-gif1.mp4"}>
               <div className="tba">
                 <p>Mint TBA</p>
               </div>
