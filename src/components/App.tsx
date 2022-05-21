20478c2f318750d69cca5b3b92ad6145700756e2
nigh7sh4de <plotnikd@gmail.com>
2022-05-21 10:24:54 -0700

    Remove app code

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,27 +1,16 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import '../styles/app.scss'
 import { HomePage } from 'src/components/home'
-// import MintPage from 'src/components/mint/MintPage'
-import WalletProvider from 'src/components/wallet/Provider'
-
 
 // eslint-disable-next-line
 declare const global: { HermesInternal: null | {} }
 
 const App = () => {
   return (
-    <div >
-      <WalletProvider>
-        <Router>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            {/* <Route path="/mint" element={<MintPage />} /> */}
-          </Routes>
-        </Router>
-      </WalletProvider>
-    </div>
+    <>
+      <HomePage />
+    </>
   )
 }
 
