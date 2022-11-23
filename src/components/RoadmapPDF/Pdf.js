23c9f72721a06431cb453bdbdbfbe08906071323
Mensheng <menshengromano@gmail.com>
2022-11-23 11:38:09 -0800

    detailed->extended

--- src/components/RoadmapPDF/Pdf.js
+++ src/components/RoadmapPDF/Pdf.js
@@ -7,7 +7,7 @@ export const Pdf = () => {
     return (
         <>
         <div className='pdfWrapper'>
-            <iframe title= "Plantoids extended Roadmap" src="https://drive.google.com/file/d/1SLS393_7WCI6qfpmgfk_tuu2cTcZJm2S/preview" allow="autoplay" ></iframe>
+            <iframe title= "Plantoids extended Roadmap" src="https://drive.google.com/file/d/1rlxZ5-hNFVPzFvgZ5buENUVVjK-7-ddC/preview" allow="autoplay" ></iframe>
         </div>
         </>
     );
