0d12acf0381cc38137aeab014b9a64eb4eb163e2
Mensheng <menshengromano@gmail.com>
2022-11-30 13:35:38 -0800

    Mint structure hosted

---
+++ src/components/premint/Premint.js
@@ -0,0 +1,14 @@
+import React from 'react';
+export const Premint = () => {
+
+
+
+
+    return (
+        <>
+        <div className='pdfWrapper'>
+            <iframe title= "Plantoids Mint Structure" src="https://drive.google.com/file/d/1T9IPVF_KXX86e0EEnFczWFsjLOCZU04Z/preview" allow="autoplay" ></iframe>
+        </div>
+        </>
+    );
+}
